3967a23a5fb1d4c068c8dfb9fa9ada7d23ae9057
Lawrence Ham <lham.staffsetter@gmail.com>
2019-03-06 15:28:36 -0700

    map test for Ash's phone o_O

--- src/app/pages/worksite/worksite.page.ts
+++ src/app/pages/worksite/worksite.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google;
 
 @Component({
   selector: 'app-worksite',
@@ -13,13 +17,15 @@ export class WorksitePage implements OnInit {
   worksite_id = null;
 
 
-  constructor(private activeRoute: ActivatedRoute) { }
+  constructor(private activeRoute: ActivatedRoute,
+              private plt: Platform,
+              private geolocation: Geolocation) { }
 
   ngOnInit() {
     this.worksite_id = this.activeRoute.snapshot.paramMap.get('id');
   }
 
-  /*
+  
   ionViewDidEnter() {
     this.plt.ready().then(() => {
       let mapOptions = {
@@ -31,7 +37,10 @@ export class WorksitePage implements OnInit {
       }
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.map.setCenter({ lat: 37.809326, lng: -122.409981 });
+      this.map.setZoom(17);
 
+/*
       this.geolocation.getCurrentPosition().then(pos => {
         let latLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
         this.map.setCenter(latLng);
@@ -40,9 +49,10 @@ export class WorksitePage implements OnInit {
       }).catch((error) => {
         console.log('Error getting location', error);
       });
+*/
     
     });
   }
-  */
+  
 
 }
